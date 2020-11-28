2d913abb594dd4e8d325d7a76264ae2895e200e9
Joseph Barr <barrjoeym@gmail.com>
2020-11-28 18:26:34 -0500

    add flight booker

--- pyimgui.py
+++ pyimgui.py
@@ -1,10 +1,17 @@
 # -*- coding: utf-8 -*-
-import glfw
-import OpenGL.GL as gl
+import datetime
 
+import glfw
 import imgui
+import OpenGL.GL as gl
 from imgui.integrations.glfw import GlfwRenderer
 
+
+def validate_date(date_str):
+    try:
+        return datetime.datetime.strptime(date_str, "%m.%d.%Y")
+    except ValueError:
+        return None
 
 def main():
     imgui.create_context()
@@ -14,6 +21,10 @@ def main():
     counter = 0
     tempC = 0
     tempF = 32
+
+    flight_current = 0
+    start_date = "01.01.1970"
+    end_date = start_date 
 
     while not glfw.window_should_close(window):
         glfw.poll_events()
@@ -59,7 +70,63 @@ def main():
         if inputF[0]:
             tempF = inputF[1]
             tempC = round(((tempF - 32) / (9/5)), 1)
+
+        imgui.end()
         
+
+
+        imgui.begin("Flight Booker")
+        num_pops = 0
+
+        _, flight_current = imgui.combo("##flight_combo", 
+                flight_current, ["one way flight", "return flight"])
+
+        start_date_date = validate_date(start_date)
+
+        # invalid date
+        if not start_date_date:
+            imgui.push_style_color(imgui.COLOR_TEXT, 1.0, 0.0, 0.0)
+            num_pops += 1
+        _, start_date = imgui.input_text('Start Date', start_date, 11)
+        
+        imgui.pop_style_color(num_pops)
+        num_pops = 0
+
+        end_date_flags = 0
+
+        end_date_date = validate_date(end_date)
+
+        # no end date for one way flight
+        if flight_current == 0:
+            end_date_flags |= imgui.INPUT_TEXT_READ_ONLY
+            imgui.push_style_color(imgui.COLOR_TEXT, 0.5, 0.5, 0.5)
+            num_pops += 1
+
+
+        # invalid date
+        if not end_date_date and flight_current == 1:
+            imgui.push_style_color(imgui.COLOR_TEXT, 1.0, 0.0, 0.0)
+            num_pops += 1
+        _, end_date = imgui.input_text('End Date', end_date, 11, end_date_flags)
+
+        imgui.pop_style_color(num_pops)
+        num_pops = 0
+
+        # PyImGui doesn't support disabled buttons, so instead conditionally show text/button
+        if not end_date_date or not start_date_date or end_date_date < start_date_date:
+            imgui.text("Book")
+        elif imgui.button("Book"):
+            imgui.open_popup("Booking Confirmation")
+        imgui.same_line()
+        if imgui.begin_popup("Booking Confirmation"):
+            if flight_current == 0:
+                imgui.text("You booked a one way flight for " + start_date_date.strftime("%m.%d.%Y")+ ".")
+            else: 
+                imgui.text("You booked a flight for " + start_date_date.strftime("%m.%d.%Y") + " and a return flight on " + end_date_date.strftime("%m.%d.%Y")+".")
+            imgui.end_popup()
+
+
+        # END FLIGHT BOOKER
         imgui.end()
 
         gl.glClearColor(1., 1., 1., 1)
